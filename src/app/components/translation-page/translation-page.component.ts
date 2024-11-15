83710b393537bb422057ee55ba3aab5eef9dfb76
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-11-15 15:22:56 -0700

    Pick random translation

--- src/app/components/translation-page/translation-page.component.ts
+++ src/app/components/translation-page/translation-page.component.ts
@@ -3,11 +3,15 @@ import {
   computed,
   ElementRef,
   inject,
+  Signal,
   ViewChild,
 } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { VideoService } from '../../services/video-service';
-import { TranslationService } from '../../services/translation.service';
+import {
+  Translation,
+  TranslationService,
+} from '../../services/translation.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-translation-page',
@@ -21,9 +25,19 @@ export class TranslationPageComponent {
   private videoService: VideoService = inject(VideoService);
   public recordedVideoElement: HTMLVideoElement | undefined;
   private translationService: TranslationService = inject(TranslationService);
+  private translations: Signal<Translation[] | undefined> = toSignal(
+    this.translationService.translations$
+  );
+
+  protected translation: Signal<Translation | undefined> = computed(() => {
+    const translations = this.translations();
+    if (translations) {
+      return this.translationService.chooseRandomTranslation(translations);
+    }
+    return undefined;
+  });
 
   ngAfterViewInit() {
-    console.log('TranslationPageComponent ngAfterViewInit');
     this.recordedVideoElement = this.recordedVideoElementRef?.nativeElement;
 
     if (this.recordedVideoElement) {
