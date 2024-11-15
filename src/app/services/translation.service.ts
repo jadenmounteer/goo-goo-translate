1318c66e0e66a694e84e18928c3b17d22060f6db
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-11-15 14:50:49 -0700

    Load the translation

--- src/app/services/translation.service.ts
+++ src/app/services/translation.service.ts
@@ -9,11 +9,28 @@ interface Translation {
   providedIn: 'root',
 })
 export class TranslationService {
-  public translationText = '';
-  constructor() {}
-  private async fetchAllTranslations(): Promise<Observable<Translation[]>> {
-    const response = await fetch('assets/trnslations.json');
+  public translationsLoading: boolean = true;
+
+  public listOfTranslations: Translation[] = [];
+
+  public translation: Translation = {} as Translation;
+
+  async ngOnInit() {
+    this.listOfTranslations = await this.fetchAllTranslations();
+    this.chooseRandomTranslation();
+    this.translationsLoading = false;
+  }
+
+  public chooseRandomTranslation(): void {
+    const randomIndex = Math.floor(
+      Math.random() * this.listOfTranslations.length
+    );
+    this.translation = this.listOfTranslations[randomIndex];
+  }
+
+  private async fetchAllTranslations(): Promise<Translation[]> {
+    const response = await fetch('assets/translations.json');
     const translations: Translation[] = await response.json();
-    return of(translations);
+    return translations;
   }
 }
