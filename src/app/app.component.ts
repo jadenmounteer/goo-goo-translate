5fad5a5c652ceb2ff62346e4b53cedb15f7747b8
Devdon <devinadonbro@gmail.com>
2024-11-15 16:07:57 -0500

    Update landing page, set logo to baked image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LogoComponent } from './logo/logo/logo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent],
+  imports: [RouterOutlet, LandingPageComponent, LogoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'goo-goo-translate';
+  protected router: Router = inject(Router);
 }
--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,16 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { LogoComponent } from '../../logo/logo/logo.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [LogoComponent],
+  imports: [],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
-  private router: Router = inject(Router);
+  protected router: Router = inject(Router);
 
   protected beginTranslation(): void {
     this.router.navigate(['/capture-video']);
